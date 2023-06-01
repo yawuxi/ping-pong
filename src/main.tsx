6a5724e1e1229586448e939c52a6d227b14dec1b
Dmytro ryliov.work@gmail.com Rylov <ryliov.work@gmail.com>
2023-06-01 14:50:04 +0200

    BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.module.scss";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
